7f4bc37d81d0d529436d8ef65647092f68c94bd3
Carlo Corvaglia <corvagliacarlo99@gmail.com>
2022-05-18 00:05:49 +0200

    Implementato movimento di base

---
+++ src/scripts/Player.gd
@@ -0,0 +1,16 @@
+extends KinematicBody2D
+
+export var speed = 20000
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var horizontal = Input.get_action_strength("right") - Input.get_action_strength("left")
+	var vertcal = Input.get_action_strength("down") - Input.get_action_strength("up")
+	var velocity = Vector2(horizontal,vertcal).normalized()*speed
+# warning-ignore:return_value_discarded
+	move_and_slide(velocity * delta)
