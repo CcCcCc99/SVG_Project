0370fa8d407a9ced587ebe8c9f4ed70c193ddfe3
Carlo Corvaglia <corvagliacarlo99@gmail.com>
2022-05-18 17:18:58 +0200

    Spostato codice in un nuovo script

--- src/scripts/Player.gd
+++ src/scripts/Player.gd
@@ -1,14 +1,6 @@
 extends KinematicBody2D
 
 export var speed = 20000
-export var distance = 50
-
-export(PackedScene) var portal
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -18,28 +10,3 @@ func _process(delta):
 	var velocity = Vector2(horizontal,vertcal).normalized()*speed
 	# warning-ignore:return_value_discarded
 	move_and_slide(velocity * delta)
-
-	# pointer rotation
-	$Arrow.look_at(get_global_mouse_position())
-	var angle = $Arrow.transform.get_rotation()
-	if angle < 0:
-		$Arrow.z_index = -1
-	else:
-		$Arrow.z_index = 0
-	
-	# spawn pointer
-	var direction = Vector2(cos(angle), sin(angle))
-	if Input.is_action_just_pressed("portal"):
-		_spawn_portal(direction)
-		
-
-var last_portal = null
-var previous_portal = null
-func _spawn_portal(direction):
-	if previous_portal != null:
-		previous_portal.queue_free()
-	previous_portal = last_portal
-	var p = portal.instance()
-	p.position = direction*distance + position
-	get_parent().add_child(p)
-	last_portal = p
---
+++ src/scripts/Portalgun.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+export var distance = 50
+export(PackedScene) var portal
+
+var last_portal = null
+var previous_portal = null
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# pointer rotation
+	look_at(get_global_mouse_position())
+	var angle = transform.get_rotation()
+	if angle < 0:
+		z_index = -1
+	else:
+		z_index = 0
+	
+	# spawn pointer
+	var direction = Vector2(cos(angle), sin(angle))
+	if Input.is_action_just_pressed("portal"):
+		_spawn_portal(direction)
+
+func _spawn_portal(direction):
+	if previous_portal != null:
+		previous_portal.queue_free()
+	previous_portal = last_portal
+	var p = portal.instance()
+	p.position = direction*distance + global_position
+	get_parent().get_parent().add_child(p)
+	last_portal = p
