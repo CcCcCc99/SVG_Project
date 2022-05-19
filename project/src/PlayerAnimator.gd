df3fea9e73f5540d04cdbc1baccdfef8a2ce3e61
Carlo Corvaglia <corvagliacarlo99@gmail.com>
2022-05-19 18:32:03 +0200

    Modificato nome cartella progetto

---
+++ project/src/PlayerAnimator.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+onready var head = $Head
+onready var body = $Body
+onready var legs = $Legs
+
+func animate(velocity):
+	if velocity == Vector2.ZERO:
+		_set_idle()
+	elif velocity.x != 0:
+		_set_horizontal(velocity.x)
+	else:
+		_set_vertical(velocity.y)
+
+func _set_idle():
+	head.flip_h = false
+	head.animation = "front"
+	body.animation = "front"
+	legs.animation = "idle"
+
+func _set_horizontal(direction):
+	head.animation = "side"
+	body.animation = "back"
+	if direction < 0:
+		head.flip_h = true
+		legs.animation = "left"
+	else:
+		head.flip_h = false
+		legs.animation = "right"
+
+func _set_vertical(direction):
+	head.flip_h = false
+	if direction < 0:
+		head.animation = "back"
+		body.animation = "back"
+	else:
+		head.animation = "front"
+		body.animation = "front"
+	legs.animation = "vertical"
---
+++ project/src/Portalgun.gd
@@ -0,0 +1,41 @@
+extends Node2D
+
+export var distance = 50
+export(PackedScene) var portal
+
+var last_portal = null
+var previous_portal = null
+var portal_number = 0
+
+
+# warning-ignore:unused_argument
+func _process(delta):
+	# pointer rotation
+	look_at(get_global_mouse_position())
+	var angle = transform.get_rotation()
+	if angle < 0:
+		z_index = -1
+	else:
+		z_index = 0
+	
+	# spawn pointer
+	var direction = Vector2(cos(angle), sin(angle))
+	if Input.is_action_just_pressed("portal"):
+		_spawn_portal(direction)
+
+func _spawn_portal(direction: Vector2):
+	var p = portal.instance()
+	p.set_color(portal_number)
+	p.position = direction*distance + global_position
+	get_parent().get_parent().add_child(p)
+	
+	if previous_portal != null:
+		previous_portal.queue_free()
+	previous_portal = last_portal
+	#previous_portal.set_destination(p)
+
+	portal_number = (portal_number+1) % 2
+	#p.set_destination(previous_portal)
+	#if previous_portal != null:
+	#	previous_portal.
+	last_portal = p
