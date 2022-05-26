32effabc167f3b02bbe2cc7834076fc50639b92d
Carlo Corvaglia <corvagliacarlo99@gmail.com>
2022-05-26 15:16:13 +0200

    Fixata sovrapposizione di portali

--- project/src/Portal.gd
+++ project/src/Portal.gd
@@ -5,24 +5,17 @@ const POOF: PackedScene = preload("res://scenes/Poof.tscn")
 var effect
 
 export(Array, Color) var colors
-var destination = null
-var is_reciving = false
+var destination: Portal2D = null
+var is_reciving: bool = false
 
 func _ready():
 	effect = POOF.instance()
 	effect.connect("animation_finished", self, "_end_effect")
 
-#func _process(delta):
-#	var areas = get_overlapping_areas()
-#	for a in areas:
-#		if a is Portal2D:
-#			a._spawn_death_effect()
-#	var bodies = get_overlapping_bodies()
-#	for b in bodies:
-#		var velocity = Vector2( 
-#			move_toward(b.position.x, self.position.x, delta), 
-#			move_toward(b.position.y, self.position.y, delta) )
-#		b.move_and_slide(velocity)
+func _physics_process(delta):
+	var areas = get_overlapping_areas()
+	if areas.has(destination):
+		_spawn_death_effect()
 
 func set_color(c: Color):
 	modulate = c
