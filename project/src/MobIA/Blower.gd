d18476e7c2302cb7bf75faf9edd07d767a08b952
Carlo Corvaglia <corvagliacarlo99@gmail.com>
2022-05-26 18:26:09 +0200

    Creata struttura cartelle

---
+++ project/src/MobIA/Blower.gd
@@ -0,0 +1,18 @@
+extends Mob
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func get_direction():
+	return Vector2(1,0)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
