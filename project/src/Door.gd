6a23c81704ee4fec3757e094beb0443f72b42457
Massimo Di Gennaro <maxilmigliore2001@gmail.com>
2022-05-27 21:05:30 +0200

    Aggiunto spostamento tra stanze

--- project/src/Door.gd
+++ project/src/Door.gd
@@ -1,6 +1,34 @@
 extends StaticBody2D
 
-onready var animation_player: AnimationPlayer = get_node("AnimationPlayer")
+onready var animated_sprite: AnimatedSprite = get_node("AnimatedSprite")
+onready var collision_shape: CollisionShape2D = get_node("CollisionShape2D")
+onready var collision_area: CollisionShape2D = get_node("Area2D/CollisionShape2D")
+
+var is_V: bool
+
+func _ready() -> void:
+	if name.ends_with("V"):
+		is_V = true
+	else:
+		is_V = false
 
 func open() -> void:
-	animation_player.play("open")
+	animated_sprite.play("open")
+	collision_shape.disabled = true
+
+func close() -> void:
+	animated_sprite.play("close")
+	collision_shape.disabled = false
+
+func _on_Area2D_body_entered(_body: KinematicBody2D) -> void:
+	if is_instance_valid(_body):
+		if is_V == false:
+			if scale.x > 0:
+				_body.position.x -= ((collision_area.shape.extents.x * 2) + (collision_shape.shape.extents.x * 2) + 20)
+			else:
+				_body.position.x += ((collision_area.shape.extents.x * 2) + (collision_shape.shape.extents.x * 2) + 20)
+		else:
+			if scale.y > 0:
+				_body.position.y -= ((collision_area.shape.extents.y * 2) + (collision_shape.shape.extents.y * 2) + 20)
+			else:
+				_body.position.y += ((collision_area.shape.extents.y * 2) + (collision_shape.shape.extents.y * 2) + 20)
