824977d7ada009d2327c55a958a6db4f40757885
Massimo Di Gennaro <maxilmigliore2001@gmail.com>
2022-05-27 21:16:24 +0200

    Completata scena stanza template e aggiunto script relativo

---
+++ project/src/Room.gd
@@ -0,0 +1,31 @@
+extends StaticBody2D
+
+var num_enemies: int
+
+onready var door_container: Node2D = get_node("Doors")
+onready var enemy_positions_container: Node2D = get_node("EnemyPositions")
+onready var player_detector: Area2D = get_node("PlayerDetector")
+
+func _ready() -> void:
+	num_enemies = enemy_positions_container.get_child_count()
+	if num_enemies == 0:
+		_open_doors()
+
+#remember to connect the signal from the mob "tree exited" to activate this
+func _on_enemy_killed() -> void:
+	num_enemies -= 1
+	if num_enemies == 0:
+		_open_doors()
+
+func _open_doors() -> void:
+	for door in door_container.get_children():
+		door.open()
+
+func _close_doors() -> void:
+	for door in door_container.get_children():
+		door.close()
+
+#func _on_PlayerDetector_body_entered(_body: KinematicBody2D) -> void:
+	#player_detector.queue_free()
+	#_close_doors()
+	#spawn enemies
