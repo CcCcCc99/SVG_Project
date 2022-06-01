04b6768548c9005dfe7ca4b4a2a95578b5bdce4f
Carlo Corvaglia <corvagliacarlo99@gmail.com>
2022-06-02 00:21:47 +0200

    Predisposto workflow per la creazione delle stanze

--- project/src/Door.gd
+++ project/src/Door.gd
@@ -4,17 +4,21 @@ onready var animated_sprite: AnimatedSprite = get_node("AnimatedSprite")
 onready var collision_shape: CollisionShape2D = get_node("CollisionShape2D")
 onready var collision_area: CollisionShape2D = get_node("Area2D/CollisionShape2D")
 
-var is_V: bool
+export var is_vertical: bool = false
+export var is_enabled: bool = false
 
 func _ready() -> void:
-	if name.ends_with("V"):
-		is_V = true
-	else:
-		is_V = false
+	if not is_enabled:
+		animated_sprite.hide()
+
+func enable() -> void:
+	animated_sprite.show()
+	is_enabled = true
 
 func open() -> void:
-	animated_sprite.play("open")
-	collision_shape.disabled = true
+	if is_enabled:
+		animated_sprite.play("open")
+		collision_shape.disabled = true
 
 func close() -> void:
 	animated_sprite.play("close")
@@ -22,7 +26,7 @@ func close() -> void:
 
 func _on_Area2D_body_entered(_body: KinematicBody2D) -> void:
 	if is_instance_valid(_body):
-		if is_V == false:
+		if is_vertical == false:
 			if scale.x > 0:
 				_body.position.x -= ((collision_area.shape.extents.x * 2) + (collision_shape.shape.extents.x * 2) + 20)
 			else:
--- project/src/Room.gd
+++ project/src/Room.gd
@@ -1,4 +1,5 @@
 extends Node2D
+class_name Room
 
 var num_enemies: int
 
@@ -6,10 +7,32 @@ onready var door_container: Node2D = get_node("Doors")
 onready var enemy_positions_container: Node2D = get_node("EnemyPositions")
 onready var player_detector: Area2D = get_node("PlayerDetector")
 
+export var closed: bool = false
+
+export var left_door_enabled: bool = false setget enable_left
+export var right_door_enabled: bool = false setget enable_right
+export var top_door_enabled: bool = false setget enable_top
+export var bottom_door_enabled: bool = false setget enable_bottom
+
+var doors
+
 func _ready() -> void:
-	num_enemies = enemy_positions_container.get_child_count()
-	if num_enemies == 0:
-		_open_doors()
+	doors = door_container.get_children()
+	_enable_doors()
+	if not closed:
+		num_enemies = enemy_positions_container.get_child_count()
+		if num_enemies == 0:
+			_open_doors()
+
+func _enable_doors():
+	if left_door_enabled:
+		doors[0].enable()
+	if right_door_enabled:
+		doors[1].enable()
+	if top_door_enabled:
+		doors[2].enable()
+	if bottom_door_enabled:
+		doors[3].enable()
 
 func _on_enemy_killed() -> void:
 	num_enemies -= 1
@@ -17,14 +40,30 @@ func _on_enemy_killed() -> void:
 		_open_doors()
 
 func _open_doors() -> void:
-	for door in door_container.get_children():
+	for door in doors:
 		door.open()
 
 func _close_doors() -> void:
-	for door in door_container.get_children():
+	for door in doors:
 		door.close()
 
 func _on_PlayerDetector_body_entered(_body: KinematicBody2D) -> void:
 	player_detector.queue_free()
 	_close_doors()
 	#_spawn_enemies()
+
+func enable_left():
+	doors[0].enable()
+	left_door_enabled = true
+
+func enable_right():
+	doors[1].enable()
+	right_door_enabled = true
+
+func enable_top():
+	doors[2].enable()
+	top_door_enabled = true
+
+func enable_bottom():
+	doors[3].enable()
+	bottom_door_enabled = true
