599826ce780d3229791d03700954a23559969777
lello <lello@mail.com>
2022-06-08 12:08:18 +0200

    Modifica posizione Mob

--- project/src/LittleFriend.gd
+++ project/src/LittleFriend.gd
@@ -19,16 +19,22 @@ func get_direction():
 					direction = Vector2(1, -1)
 				elif (position.x > 0 and position.y > 0):
 					direction = Vector2(-1, -1)
-				if (position.x > -10 and position.x < 10 and position.y > -10 and position.y < 10):
+				if (position.x > -20 and position.x < 20 and position.y > -90 and position.y < 90):
 					is_returning = false
 					get_parent().is_returned = true
+					position = Vector2.ZERO
+					direction = Vector2(-1, -1)
+					return Vector2.ZERO
 			return direction
 		IDLE:
 			$AnimatedSprite.animation = "idle"
 			return Vector2.ZERO
 
 func _on_collision_environment():
-	direction *= -1
+	if (not is_returning):
+		get_parent().direction.y *= -1
+	else:
+		direction.y *= -1
 
 func _on_Cooldown_timeout():
 	ia_state = IDLE
