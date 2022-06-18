cd2a6c6a7aeb945d4619d9c73f33b1ef340f6bf4
Angela Gallo <angelagallo22@gmail.com>
2022-06-18 11:20:41 +0200

    implementato attacco

--- project/src/MobIA/Prism.gd
+++ project/src/MobIA/Prism.gd
@@ -10,16 +10,28 @@ func get_direction():
 			_walk()
 			return direction
 		ATTACK:
+			$Sprite.hide()
+			_laser_attack()
 			return Vector2.ZERO
 
 func _on_collision_environment():
 	direction.x *= -1
 	scale.x *= -1
+	ia_state = ATTACK
 
 func _walk():
 	i += 0.08
 	$Sprite.position.y += sin(i)
 
+func _laser_attack():
+	$AttackMode/RayCast2D.play_laser()
+	$AttackMode.rotation += 0.07
+	if $AttackMode.rotation >= 2*PI:
+		ia_state = WALK
+		$AttackMode.rotation = 0
+		$AttackMode.hide()
+
+"""
 func _on_TriggerAttack(body):
 	if ia_state != ATTACK:
 		ia_state = ATTACK
@@ -31,7 +43,7 @@ func _on_TriggerAttack(body):
 			j.set_direction(Vector2(-1,0))
 		j.position = $TriggerAttack.global_position - Vector2(30,15)
 		get_parent().call_deferred("add_child",j)
-
+"""
 
 func _on_Cooldown_timeout():
 	ia_state = WALK
