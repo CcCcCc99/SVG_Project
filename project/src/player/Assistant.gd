0621b71fbcea8d3c68adf01f9959c7cdcedcb15d
Carlo Corvaglia <corvagliacarlo99@gmail.com>
2022-06-21 17:24:25 +0200

    Implementato sistema del mana

--- project/src/player/Assistant.gd
+++ project/src/player/Assistant.gd
@@ -2,10 +2,12 @@ extends Node2D
 
 class summon:
 	var mob: Mob
+	var mana_cost: int
 	var summoned: bool
 
-	func _init(m):
+	func _init(m, c):
 		mob = m
+		mana_cost = c
 		summoned = false
 
 	func spawn(parent: Node, pos: Vector2):
@@ -14,12 +16,21 @@ class summon:
 			mob.is_summoned = true
 			mob.position = pos
 			mob.modulate = Color.darkblue
+			mob.get_node("Shadow").hide()
 			parent.add_child(mob)
 
 var summons: Array = [null, null, null, null, null, null]
-export var summons_number = 1
+export var summons_number: int = 1
+
+export(int) var max_mana: int
+var mana: int = 0 setget set_mana, get_mana
+
+signal mana_changed(old_mana, new_mana)
 
 var action_bar setget set_actionbar
+
+func _ready():
+	set_mana(max_mana)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -27,14 +38,28 @@ func _process(delta):
 		summons[action_bar.current()].spawn(
 			get_parent().get_node("Room").get_node("Objects"),
 			get_global_mouse_position())
+		set_mana(mana - summons[action_bar.current()].mana_cost)
 
 func set_actionbar(bar):
 	action_bar = bar
 
-func add_summon(sum):
-	summons[action_bar.current()] = summon.new(sum)
+func add_summon(sum, cost):
+	summons[action_bar.current()] = summon.new(sum, cost)
 	var t = sum.icon
 	action_bar.set_slot(t)
 
 func add_slot():
 	pass
+
+func set_mana(new_mana: int):
+	var old_mana = get_mana()
+	if old_mana != new_mana:
+		mana = int(clamp(new_mana, 0, max_mana))
+		emit_signal("mana_changed", old_mana, get_mana())
+
+func get_mana() -> int:
+	return mana
+
+func set_max_mana(new_max_mana: int):
+	max_mana = new_max_mana
+	set_mana(new_max_mana)
--- project/src/player/Player.gd
+++ project/src/player/Player.gd
@@ -2,14 +2,7 @@ extends Character
 
 onready var anim = $Animator
 
-export(int) var max_mana: int
-var mana: int setget set_mana, get_mana
-
 signal hp_changed(old_hp, new_hp)
-signal mana_changed(old_mana, new_mana)
-
-func _ready():
-	set_mana(max_mana)
 
 func _process(delta):
 	._process(delta)
@@ -31,19 +24,6 @@ func get_direction() -> Vector2:
 	var horizontal = Input.get_action_strength("right") - Input.get_action_strength("left")
 	var vertcal = Input.get_action_strength("down") - Input.get_action_strength("up")
 	return Vector2(horizontal,vertcal).normalized()
-
-func set_mana(new_mana: int):
-	var old_mana = get_mana()
-	if old_mana != new_mana:
-		mana = clamp(new_mana, 0, max_mana)
-		emit_signal("mana_changed", old_mana, get_mana())
-
-func get_mana() -> int:
-	return mana
-
-func set_max_mana(new_max_mana: int):
-	max_mana = new_max_mana
-	set_mana(new_max_mana)
 
 func _on_Bat_area_entered(area):
 	pass # Replace with function body.
