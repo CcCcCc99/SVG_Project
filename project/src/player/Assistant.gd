ae15cc6cf99470ed6873f58fc0feaf640baab2ed
Carlo Corvaglia <corvagliacarlo99@gmail.com>
2022-06-21 12:52:39 +0200

    Fixato punto di spawn della summon

--- project/src/player/Assistant.gd
+++ project/src/player/Assistant.gd
@@ -7,11 +7,11 @@ var action_bar setget set_actionbar
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("summon"):
+	if Input.is_action_just_pressed("summon") and is_instance_valid(summons[action_bar.current()]):
 		print("summon ", get_viewport().get_mouse_position())
 		summons[action_bar.current()].is_summoned = true
-		summons[action_bar.current()].position = get_viewport().get_mouse_position()
-		get_parent().add_child(summons[action_bar.current()])
+		summons[action_bar.current()].position = get_global_mouse_position()
+		get_parent().get_node("Room").get_node("Objects").add_child(summons[action_bar.current()])
 
 func set_actionbar(bar):
 	action_bar = bar
