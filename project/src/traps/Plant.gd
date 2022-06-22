a0ee15fd7643cd305db7f75d70fb31b4bf34c0ac
Massimo Di Gennaro <maxilmigliore2001@gmail.com>
2022-06-22 14:14:01 +0200

    Nuova versione della pianta carnivora

--- project/src/traps/Plant.gd
+++ project/src/traps/Plant.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 export var damage = 3
 
-var body
+var areas_array: Array
 
 func _on_Plant_area_entered(area):
 	if $Timer.time_left == 0:
@@ -14,31 +14,29 @@ func _on_hit(area):
 			$AnimatedSprite.animation = "eat"
 			$AnimatedSprite.frame = 0
 			
-			body = area
+		if not areas_array.has(area):
+			areas_array.append(area)
 
 func _on_AnimatedSprite_animation_finished():
 	if $AnimatedSprite.animation == "eat":
 		$Timer.start()
 		$AnimatedSprite.animation = "back"
 		
-		if is_instance_valid(body):
-			if body.is_in_group("Character"):
-				body.take_damage(damage)
-			elif body.is_in_group("Portal"):
-				#mangia il portale
-				body._spawn_death_effect()
+		for body in areas_array:
+			if is_instance_valid(body):
+				if body.is_in_group("Character"):
+					body.take_damage(damage)
+				elif body.is_in_group("Portal"):
+					#mangia il portale
+					body._spawn_death_effect()
+		
+		areas_array.clear()
 	elif $AnimatedSprite.animation == "back":
 		$AnimatedSprite.animation = "idle"
 
 func _on_Timer_timeout():
-	pass
-	#var min_pos_x = $CollisionShape2D.global_position.x - $CollisionShape2D.shape.extents.x
-	#var max_pos_x = $CollisionShape2D.global_position.x + $CollisionShape2D.shape.extents.x
-	#var min_pos_y = $CollisionShape2D.global_position.y - $CollisionShape2D.shape.extents.y
-	#var max_pos_y = $CollisionShape2D.global_position.y + $CollisionShape2D.shape.extents.y
-	#
-	#if is_instance_valid(body):
-		#if (body.global_position.x >= min_pos_x) and (body.global_position.x <= max_pos_x) and (body.global_position.y >= min_pos_y) and (body.global_position.y <= max_pos_y):
-			#_on_Plant_area_entered(body)
-		#else:
-			#body = null
+	var areas = get_overlapping_areas()
+	if areas.size() != 0:
+		for area in areas:
+			if area.is_in_group("Hitbox") or area.is_in_group("Portal"):
+				_on_hit(area)
