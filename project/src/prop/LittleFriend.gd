b73d76f10882e58f2541f806b6eefbf097f2ccb6
lello <lello@mail.com>
2022-06-22 10:03:10 +0200

    Correzione errori

--- project/src/prop/LittleFriend.gd
+++ project/src/prop/LittleFriend.gd
@@ -1,15 +1,15 @@
 extends Shot
 
-export var bounces: int = 4
+export var bounces: int = 10
 var cont: int = 0
 
 func _on_hit(body):
 	if body.is_in_group("Environment"):
-		_bouce()
+		_bounce()
 	else:
 		._on_hit(body)
 
-func _bouce():
+func _bounce():
 	if cont < bounces:
 		direction.y *= -1
 		cont += 1
