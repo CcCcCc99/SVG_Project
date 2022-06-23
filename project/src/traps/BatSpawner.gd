20d5016bd0a47f7468eac4372d277db9934739bb
Massimo Di Gennaro <maxilmigliore2001@gmail.com>
2022-06-23 21:52:50 +0200

    Risolti altri bug di minore intensità

--- project/src/traps/BatSpawner.gd
+++ project/src/traps/BatSpawner.gd
@@ -1,22 +1,37 @@
 extends Sprite
+
 export(PackedScene) var bat
 export var flipx: bool
 export var flipy: bool
 
 func _ready():
+	if flipx && flipy:
+		self.offset.y = -350
+	elif flipx && !flipy:
+		self.flip_h = true
+		self.offset.x = 350
+	elif !flipx && flipy:
+		self.flip_v = true
+		self.offset.x = -350
+	else:
+		self.flip_h = true
+		self.flip_v = true
+		self.offset.y = 350
+	
 	$Timer.start()
 
 func _on_Timer_timeout():
 	var b = bat.instance()
 	if flipx && flipy:
-		#b.set_direction(Vector2(1,0))
-		b.set_direction(Vector2(0,1))
+		# b.set_direction(Vector2(1, 0))
+		b.set_direction(Vector2(0, 1))
 	elif flipx && !flipy:
-		b.set_direction(Vector2(-1,0))
+		b.set_direction(Vector2(-1, 0))
 	elif !flipx && flipy:
-		b.set_direction(Vector2(1,0))
+		b.set_direction(Vector2(1, 0))
 	else:
-		b.set_direction(Vector2(0,-1))
+		b.set_direction(Vector2(0, -1))
+	
 	b.get_node("AnimatedSprite").flip_v = false
 	b.position = global_position
 	get_parent().add_child(b)
--- project/src/traps/Plant.gd
+++ project/src/traps/Plant.gd
@@ -2,41 +2,41 @@ extends Area2D
 
 export var damage = 3
 
-var areas_array: Array
+var objects_array: Array
 
-func _on_Plant_area_entered(area):
+func _on_hit(object):
 	if $Timer.time_left == 0:
-		_on_hit(area)
-
-func _on_hit(area):
-	if not area.is_in_group("Environment"):
-		if $AnimatedSprite.animation == "idle":
-			$AnimatedSprite.animation = "eat"
-			$AnimatedSprite.frame = 0
-			
-		if not areas_array.has(area):
-			areas_array.append(area)
+		if not object.is_in_group("Environment"):
+			if $AnimatedSprite.animation == "idle":
+				$AnimatedSprite.animation = "eat"
+				$AnimatedSprite.frame = 0
+				
+			if not objects_array.has(object):
+				objects_array.append(object)
 
 func _on_AnimatedSprite_animation_finished():
 	if $AnimatedSprite.animation == "eat":
 		$Timer.start()
 		$AnimatedSprite.animation = "back"
 		
-		for body in areas_array:
-			if is_instance_valid(body):
-				if body.is_in_group("Character"):
-					body.take_damage(damage)
-				elif body.is_in_group("Portal"):
-					#mangia il portale
-					body._spawn_death_effect()
+		for object in objects_array:
+			if is_instance_valid(object):
+				if object.is_in_group("Character"):
+					object.take_damage(damage)
+				elif object.is_in_group("Portal"):
+					# mangia il portale
+					object._spawn_death_effect()
 		
-		areas_array.clear()
+		objects_array.clear()
 	elif $AnimatedSprite.animation == "back":
 		$AnimatedSprite.animation = "idle"
 
 func _on_Timer_timeout():
 	var areas = get_overlapping_areas()
+	var bodies = get_overlapping_bodies()
+	areas.append_array(bodies)
+	
 	if areas.size() != 0:
-		for area in areas:
-			if area.is_in_group("Hitbox") or area.is_in_group("Portal"):
-				_on_hit(area)
+		for object in areas:
+			if object.is_in_group("Character") or object.is_in_group("Portal"):
+				_on_hit(object)
