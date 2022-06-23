90ae236e1172932591014743bdd49a2e1abcec9d
Carlo Corvaglia <corvagliacarlo99@gmail.com>
2022-06-23 18:56:24 +0200

    Abbozzato sistema dei volumi

--- project/src/default_load.gd
+++ project/src/default_load.gd
@@ -1,8 +1,13 @@
 extends Node
 
-var music_volume: float = 10
-var sfx_volume: float = 10
-var master_volume: float = 10
+var master_volume: float = 0
+var sfx_volume: float = 0
+var music_volume: float = 0
 
 var resolution: Vector2
 var fullscreen: bool
+
+func _ready():
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), master_volume)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), sfx_volume)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), music_volume)
