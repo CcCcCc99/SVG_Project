0d5a2c6d7ef4075c5f129b4d88413bcbda4f647a
Massimo Di Gennaro <maxilmigliore2001@gmail.com>
2022-06-24 15:32:21 +0200

    Definita risoluzione e relativi controlli

--- project/src/default_load.gd
+++ project/src/default_load.gd
@@ -11,6 +11,29 @@ func _ready() -> void:
 	_load_config()
 	_set_config()
 
+func set_resolution(res: Vector2) -> void:
+	if resolution != res:
+		resolution = res
+		
+		if resolution < OS.get_screen_size():
+			OS.set_window_maximized(false)
+			OS.set_window_size(resolution)
+		else:
+			if !OS.is_window_maximized():
+				OS.set_window_size(OS.get_screen_size())
+				OS.set_window_maximized(true)
+		OS.center_window()
+		
+		_save_config()
+
+func set_fullscreen(fs: bool) -> void:
+	if fullscreen != fs:
+		fullscreen = fs
+		
+		OS.set_window_full_screen(fullscreen)
+		
+		_save_config()
+
 func _save_config() -> void:
 	var file = File.new()
 	file.open("config.ini", File.WRITE)
@@ -39,5 +62,12 @@ func _load_config() -> void:
 
 func _set_config() -> void:
 	# volumi da settare #
-	OS.set_window_size(resolution)
+	OS.set_window_resizable(false)
+	if resolution < OS.get_screen_size():
+		OS.set_window_maximized(false)
+		OS.set_window_size(resolution)
+	else:
+		OS.set_window_size(OS.get_screen_size())
+		OS.set_window_maximized(true)
+	OS.center_window()
 	OS.set_window_fullscreen(fullscreen)
