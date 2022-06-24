3d38adb7907b2fa51cc17e55dad5bc9d19183403
Massimo Di Gennaro <maxilmigliore2001@gmail.com>
2022-06-24 12:12:44 +0200

    Aggiunte configurazioni

--- project/src/default_load.gd
+++ project/src/default_load.gd
@@ -1,8 +1,43 @@
 extends Node
 
-var music_volume: float = 10
-var sfx_volume: float = 10
-var master_volume: float = 10
+var music_volume: float
+var sfx_volume: float
+var master_volume: float
 
 var resolution: Vector2
 var fullscreen: bool
+
+func _ready() -> void:
+	_load_config()
+	_set_config()
+
+func _save_config() -> void:
+	var file = File.new()
+	file.open("config.ini", File.WRITE)
+	
+	file.store_var(music_volume)
+	file.store_var(sfx_volume)
+	file.store_var(master_volume)
+	
+	file.store_var(resolution)
+	file.store_var(fullscreen)
+	
+	file.close()
+
+func _load_config() -> void:
+	var file = File.new()
+	file.open("config.ini", File.READ)
+	
+	music_volume = file.get_var()
+	sfx_volume = file.get_var()
+	master_volume = file.get_var()
+	
+	resolution = file.get_var()
+	fullscreen = file.get_var()
+	
+	file.close()
+
+func _set_config() -> void:
+	# volumi da settare #
+	OS.set_window_size(resolution)
+	OS.set_window_fullscreen(fullscreen)
