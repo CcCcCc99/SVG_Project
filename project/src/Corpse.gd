180603358b707f8faeb1c3ff4cbbaec00f956bdb
Carlo Corvaglia <corvagliacarlo99@gmail.com>
2022-06-27 17:03:16 +0200

    Implementato output allo spawn

--- project/src/Corpse.gd
+++ project/src/Corpse.gd
@@ -9,4 +9,4 @@ func _on_Corpse_input_event(viewport, event, shape_idx):
 		if event.is_action_pressed("teke_soul") and not is_soul_taken:
 			$Shadow.hide()
 			is_soul_taken = true
-			get_tree().root.get_child(0).load_summon(summon, cost)
+			get_tree().root.get_node("Main").load_summon(summon, cost)
