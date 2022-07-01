86490dc5912603e0c110831c2f2252e19c22eca9
Angela Gallo <angelagallo22@gmail.com>
2022-07-01 19:23:39 +0200

    creato sistema di cambio livello

--- project/src/GameManager.gd
+++ project/src/GameManager.gd
@@ -8,10 +8,18 @@ var assistant
 onready var health_bar = get_node("HUD/HealthBar")
 onready var mana_bar = get_node("HUD/ManaBar")
 
-var testlLevel = preload("res://levels/TestLevel.tres")
+#var testlLevel #= preload("res://levels/TestLevel.tres")
+var levels: Array
+var currentLevel
+
+export var loadlvl = 0
 
 var rooms: Array
 var current_room: int
+
+func _init():
+	levels.append("res://levels/TestLevel.tres")
+	levels.append("res://levels/TestLevel2.tres")
 
 func _ready():
 	player = player_scene.instance()
@@ -20,14 +28,26 @@ func _ready():
 	health_bar.set_player(player)
 	mana_bar.set_player(assistant)
 	add_child(assistant)
-	_load_level()
+	_load_level(loadlvl)
 
-func _load_level():
-	var room_scenes = testlLevel.get_rooms()
+func _load_level(l: int):
+	currentLevel = load(levels[l])
+	rooms.clear()
+	var room_scenes = currentLevel.get_rooms()
 	for rs in room_scenes:
 		rooms.append(rs.instance())
-	var start = testlLevel.get_first_room()
+	var start = currentLevel.get_first_room()
 	_load_room(start, 0)
+
+func _unload_level():
+	_unload_room()
+	#currentLevel.queue_free()
+	pass
+
+func _switch_to_level(l: int):
+	#call_deferred("_unload_room")
+	call_deferred("_unload_level")
+	call_deferred("_load_level", l)
 
 func _load_room(r: int, d: int):
 	current_room = r
@@ -38,17 +58,16 @@ func _load_room(r: int, d: int):
 	rooms[r].set_player_position(player, d)
 
 func _unload_room():
+	player.destroy_portals()
+	assistant.destroy_summons()
 	rooms[current_room].get_node("Objects").remove_child(player)
 	rooms[current_room].disconnect("exited_room", self, "_switch_to_room")
 	remove_child(rooms[current_room])
 
 func _switch_to_room(r: int, d: int):
-	player.destroy_portals()
-	assistant.destroy_summons()
 	call_deferred("_unload_room")
 	call_deferred("_load_room", r, d)
 
 func load_summon(sum, cost):
 	var summon = load(sum).instance()
 	assistant.add_summon(summon, cost)
-
