f122d20fb807ca8b97cbc82c8b18bac5791cece5
Carlo Corvaglia <corvagliacarlo99@gmail.com>
2022-07-01 17:47:37 +0200

    Cambiato tipo raccoglibile

--- project/src/hud/HealthGain.gd
+++ project/src/hud/HealthGain.gd
@@ -1,27 +1,19 @@
-extends StaticBody2D
+extends RigidBody2D
 
-var speed = 0
+export var decelleration = Vector2(0.5, 0.5)
 var direction: Vector2 = Vector2.ZERO
 
 func _physics_process(delta):
-	position += direction * speed * delta
+	print(linear_velocity)
+	if linear_velocity > Vector2.ZERO:
+		linear_velocity -= decelleration
+		if linear_velocity <= Vector2.ZERO:
+			linear_velocity = Vector2.ZERO
 
 func _on_Area2DGain_body_entered(body):
 	if body.is_in_group("Player"):
 		var hp = body.get_hp()
-		body.set_hp(body.get_hp() + 1)
+		body.set_hp(body.get_hp() + 2)
 		if (hp != body.get_hp()):
 			self.queue_free()
 
-
-func _on_Area2DPush_body_entered(body):
-	if body.is_in_group("Player"):
-		speed = 1000
-		direction.x = -body.get_direction().x
-		direction.y = -body.get_direction().y
-	elif body.is_in_group("HorizontalEnvironment"):
-		speed -= 100
-		direction.x *= -1
-	elif body.is_in_group("VerticalEnvironment"):
-		speed -= 100
-		direction.y *= -1
