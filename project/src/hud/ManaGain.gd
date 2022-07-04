c15d2c3174bfc12f37d6ba0241e8e9429d85ac5f
lello <lello@mail.com>
2022-07-04 21:33:40 +0200

    Modificato managain

--- project/src/hud/ManaGain.gd
+++ project/src/hud/ManaGain.gd
@@ -1,11 +1,15 @@
-extends StaticBody2D
+extends RigidBody2D
 
 var assistant
-var speed = 0
+export var decelleration = Vector2(0.5, 0.5)
 var direction: Vector2 = Vector2.ZERO
 
 func _physics_process(delta):
-	position += direction * speed * delta
+	print(linear_velocity)
+	if linear_velocity > Vector2.ZERO:
+		linear_velocity -= decelleration
+		if linear_velocity <= Vector2.ZERO:
+			linear_velocity = Vector2.ZERO
 
 func _on_Area2DGain_body_entered(body):
 	if body.is_in_group("Player"):
@@ -13,18 +17,6 @@ func _on_Area2DGain_body_entered(body):
 		assistant.set_mana(mana + 1)
 		if(mana != assistant.get_mana()):
 			self.queue_free()
-
-func _on_Area2DPush_body_entered(body):
-	if body.is_in_group("Player"):
-		speed = 1000
-		direction.x = -body.get_direction().x
-		direction.y = -body.get_direction().y
-	elif body.is_in_group("HorizontalEnvironment"):
-		speed -= 100
-		direction.x *= -1
-	elif body.is_in_group("VerticalEnvironment"):
-		speed -= 100
-		direction.y *= -1
 
 func set_assistant(assistant):
 	self.assistant = assistant
