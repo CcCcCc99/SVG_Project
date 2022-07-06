dd787ba80a5d5e2508248ee0d0b9334cbf6df328
Carlo Corvaglia <corvagliacarlo99@gmail.com>
2022-07-06 14:41:33 +0200

    Generalizzata ricompensa

--- project/src/prop/Pickupable.gd
+++ project/src/prop/Pickupable.gd
@@ -1,12 +1,11 @@
 extends RigidBody2D
-class_name pickupable
+class_name Pickupable
 
 export var decelleration = 0.7#Vector2(0.5,0.5)
 export var limit = 30
 var direction: Vector2 = Vector2.ZERO
 
 func _physics_process(delta):
-	print(linear_velocity)
 	if linear_velocity.length() > 0:
 		linear_velocity *= decelleration
 		if linear_velocity.length() <= limit:
@@ -14,8 +13,7 @@ func _physics_process(delta):
 
 func _on_Area2DGain_body_entered(body):
 	if body.is_in_group("Player"):
-		var hp = body.get_hp()
-		body.set_hp(body.get_hp() + 2)
-		if (hp != body.get_hp()):
-			self.queue_free()
+		_effect(body)
 
+func _effect(player: Character):
+	pass
--- project/src/prop/PortalReward.gd
+++ project/src/prop/PortalReward.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends Node2D
 
 export(PackedScene) var healthScene
 export(PackedScene) var manaScene
