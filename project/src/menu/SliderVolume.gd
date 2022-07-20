0b5a764330fe46572c72c335c51a59b01983180e
Angela Gallo <angelagallo22@gmail.com>
2022-07-20 16:35:10 +0200

    corretto cambiamento volume

--- project/src/menu/SliderVolume.gd
+++ project/src/menu/SliderVolume.gd
@@ -3,11 +3,15 @@ extends HSlider
 
 export var audio_bus_name := "Master"
 
-onready var _bus := AudioServer.get_bus_index(audio_bus_name)
+#onready var _bus := AudioServer.get_bus_index(audio_bus_name)
 
 
 func _ready() -> void:
-	value = db2linear(AudioServer.get_bus_volume_db(_bus))
-
-func _on_value_changed(value):
-	AudioServer.set_bus_volume_db(_bus, linear2db(value))
+	#value = db2linear(AudioServer.get_bus_volume_db(_bus))
+	match audio_bus_name:
+		"Master":
+			value = get_tree().root.get_child(0).master_volume
+		"SFX":
+			value = get_tree().root.get_child(0).sfx_volume
+		"Music":
+			value = get_tree().root.get_child(0).music_volume
--- project/src/menu/VolumeScreen.gd
+++ project/src/menu/VolumeScreen.gd
@@ -10,3 +10,15 @@ func _press_button_number(num: int):
 
 func _on_Back_pressed():
 	get_parent().enable()
+
+
+func _on_Master_value_changed(value):
+	get_tree().root.get_child(0).set_volume("Master", value)
+
+
+func _on_SoundEffects_value_changed(value):
+	get_tree().root.get_child(0).set_volume("SFX", value)
+
+
+func _on_BackgroundSound_value_changed(value):
+	get_tree().root.get_child(0).set_volume("Music", value)
