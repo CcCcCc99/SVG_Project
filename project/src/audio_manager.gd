142ffe7e2895b01166fdeb842dd8e6969146257e
Massimo Di Gennaro <maxilmigliore2001@gmail.com>
2022-08-03 16:54:56 +0200

    Aggiunto suono allo spawn dei nemici

--- project/src/audio_manager.gd
+++ project/src/audio_manager.gd
@@ -3,22 +3,16 @@ extends Node
 var effect = preload("res://scenes/Effect.tscn")
 
 var musics
-var effects
 
 func _ready() -> void:
 	var m = Node.new()
 	m.name = "Musics"
 	add_child(m)
 	musics = m
-	
-	var e = Node.new()
-	e.name = "Effects"
-	add_child(e)
-	effects = e
 
 func add_effect(audio: String, volume: float, pitch: float, loop: bool) -> void:
 	var new_effect = effect.instance()
-	effects.add_child(new_effect)
+	add_child(new_effect)
 	new_effect.set_effect(audio, volume, pitch, loop)
 
 func add_music(audio: String, volume: float, pitch: float) -> void:
