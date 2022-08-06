cc479928bc38c081bbb929319aac58684fb1e287
Carlo Corvaglia <corvagliacarlo99@gmail.com>
2022-08-06 19:05:09 +0200

    Implementato inseguimento giocatore

--- project/src/MobIA/Taz.gd
+++ project/src/MobIA/Taz.gd
@@ -1,8 +1,10 @@
 extends Mob
 
 export var direction: Vector2
+export var knock_back: float = 5
 var last_env: Node
 var prev_dir: Vector2
+var target: Character = null
 
 func _ready():
 	prev_dir = direction
@@ -12,20 +14,24 @@ func _ready():
 func get_direction() -> Vector2:
 	var dir
 	match ia_state:
+		IDLE:
+			dir = Vector2.ZERO
 		WALK:
 			_walk()
 			dir = direction
 		ATTACK:
-			_tornado_attack()
 			dir = _follow_player()
-	if prev_dir.x != dir.x:
+	if (prev_dir.x > 0 and dir.x < 0) or (prev_dir.x < 0 and dir.x > 0):
 		scale.x *= -1
 		prev_dir = dir
 	return dir
 
 func _on_BodyChecker_body_entered(body):
-	._on_BodyChecker_body_entered(body)
 	last_env = body
+	if body.is_in_group("Character") and not body.is_in_group("Hitbox") and body != self:
+		ia_state = IDLE
+		_fall()
+	._on_BodyChecker_body_entered(body)
 
 func _on_collision_environment():
 	if last_env.is_in_group("VerticalEnvironment"):
@@ -39,6 +45,13 @@ func _bounce(horizontal):
 	else:
 		direction.y *= -1
 
+func _fall():
+	$AnimatedSprite.hide()
+	$Shadow.hide()
+	$FallenMode.show()
+	$FallenShadow.show()
+	$Fall_AnimationPlayer.play("FallBack")
+
 func _walk():
 	$AnimatedSprite.show()
 	$Shadow.show()
@@ -47,11 +60,13 @@ func _walk():
 	i += 0.08
 	$AnimatedSprite.position.y += sin(i)
 
-func _tornado_attack():
-	$AnimatedSprite.hide()
-	$Shadow.hide()
-	$FallenMode.show()
-	$FallenShadow.show()
-
 func _follow_player() -> Vector2:
-	return Vector2.ZERO
+	return position.direction_to(target.position)
+
+func _on_TriggerAttack_enemy_spotted(body):
+	ia_state = ATTACK
+	$AnimatedSprite.animation = "spin"
+	if body.is_in_group("Hitbox"):
+		target = body.get_character()
+	else:
+		target = body
