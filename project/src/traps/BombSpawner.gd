6bfff5ccc9fcd371b13171341e4ebcddc5d6cda2
Massimo Di Gennaro <maxilmigliore2001@gmail.com>
2022-08-12 21:21:00 +0200

    Aggiunto suono alla ragnatela

--- project/src/traps/BombSpawner.gd
+++ project/src/traps/BombSpawner.gd
@@ -2,15 +2,16 @@ extends AnimatedSprite
 
 export(PackedScene) var bomb
 
-onready var max_children: int = get_parent().get_child_count() + 1
+var actual_bomb: Node
 
 func _ready():
 	$Timer.start()
-	get_node("/root/AudioManager").add_effect("res://assets/audio/41254196_sewing-machine-slow-speed-01.mp3", -10.0, 0.5, true)
+	get_node("/root/AudioManager").add_effect("res://assets/audio/41254196_sewing-machine-slow-speed-01.mp3", 0.0, 0.5, true)
 
 func _on_Timer_timeout():
-	if get_parent().get_child_count() < max_children:
+	if not is_instance_valid(actual_bomb):
 		var b = bomb.instance()
 		b.position.x = global_position.x - 40
 		b.position.y = global_position.y + 60
 		get_parent().add_child(b)
+		actual_bomb = b
