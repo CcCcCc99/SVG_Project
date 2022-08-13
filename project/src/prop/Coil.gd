3301359f213488156a3b8bb4f6059c6728b475fb
Angela Gallo <angelagallo22@gmail.com>
2022-08-13 18:33:18 +0200

    completate bobine

--- project/src/prop/Coil.gd
+++ project/src/prop/Coil.gd
@@ -3,16 +3,13 @@ extends Node2D
 export var on_color: Color
 export var off_color: Color
 export (Array, String) var line_num
+export (Array, String) var neighbors
 
-func checkline(neighbors: Array):
-	print(neighbors)
+func checkline():
 	for i in line_num.size():
 		var line = "Line" + line_num[i]
 		var coil = "Coil" + neighbors[i] + "/AnimatedSprite"
-		print("Line: " + line + " Coil: " + coil + " Self: " + self.name)
-		print("CoilState: " + get_parent().get_node(coil).animation + "SelfState: " + $AnimatedSprite.animation)
 		if $AnimatedSprite.animation == "on" and get_parent().get_node(coil).animation == "on":
 			get_parent().get_node(line).default_color = on_color
 		else:
 			get_parent().get_node(line).default_color = off_color
-		print(get_parent().get_node(line).default_color)
--- project/src/prop/Levers/LeverCoil.gd
+++ project/src/prop/Levers/LeverCoil.gd
@@ -3,14 +3,12 @@ extends Lever
 export (Array, String) var coil_num
 
 func _activate():
-	print("Leva: " + self.name)
 	for x in coil_num:
 		var coil = "Coil" + x + "/AnimatedSprite"
 		var line = "Coil" + x
 		checkcoil(coil, line)
 
 func _deactivate():
-	print("Leva: " + self.name)
 	for x in coil_num:
 		var coil = "Coil" + x + "/AnimatedSprite"
 		var line = "Coil" + x
@@ -21,4 +19,4 @@ func checkcoil(coil: String, line: String):
 		get_parent().get_node(coil).animation = "off"
 	else:
 		get_parent().get_node(coil).animation = "on"
-	get_parent().get_node(line).checkline(coil_num)
+	get_parent().get_node(line).checkline()
