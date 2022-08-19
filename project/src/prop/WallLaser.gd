9c89c672562860d273229dcc211f77c36f1aef68
Carlo Corvaglia <corvagliacarlo99@gmail.com>
2022-08-19 14:00:53 +0200

    Fix bug del portale oltre la linea

--- project/src/prop/WallLaser.gd
+++ project/src/prop/WallLaser.gd
@@ -1,20 +1,29 @@
 extends Line2D
 
-const offset = Vector2(-14, 76)
-var segment: SegmentShape2D 
+const hit_offset = Vector2(-14, 76)
+var pos_offset = Vector2.ZERO
+
+var segment: SegmentShape2D
+var player: Character
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	player = get_parent().get_node("Player")
 	$StaticBody/CollisionShape2D.shape = $StaticBody/CollisionShape2D.shape.duplicate()
 	segment = $StaticBody/CollisionShape2D.shape
-	segment.set_a(self.points[0] + offset)
-	segment.set_b(self.points[1] + offset)
+	_set_points()
 
+func _set_points():
+	points[0] -= pos_offset
+	points[1] -= pos_offset
+	segment.set_a(self.points[0] + hit_offset)
+	segment.set_b(self.points[1] + hit_offset)
+	print(points)
 
 func turn_on():
 	modulate = Color.white
-	segment.set_b(self.points[1] + offset)
+	segment.set_b(self.points[1] + hit_offset)
 
 func turn_off():
 	modulate = Color.transparent
-	segment.set_b(self.points[0] + offset)
+	segment.set_b(self.points[0] + hit_offset)
