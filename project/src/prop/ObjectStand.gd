809d921f241b1ea28897d63834be6e0f3751818d
Massimo Di Gennaro <maxilmigliore2001@gmail.com>
2022-08-29 17:45:39 +0200

    Migliorato lo stand

--- project/src/prop/ObjectStand.gd
+++ project/src/prop/ObjectStand.gd
@@ -11,6 +11,8 @@ func start():
 func _on_ObjectStand_body_entered(body):
 	if body.is_in_group("Player") and has_object:
 		remove_object()
+		if is_instance_valid(object) and not body.has_node("FlyingDiamond"):
+			body.add_child(object.instance())
 		emit_signal("object_collected")
 
 func remove_object():
