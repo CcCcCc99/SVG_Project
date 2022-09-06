c59fb2a8f84df37281a7a730e5459175507b36ae
Carlo Corvaglia <corvagliacarlo99@gmail.com>
2022-09-07 01:43:26 +0200

    Risolti problemi di merge

--- project/scenes/Rooms/Lv1Rooms/crepa.gd
+++ project/scenes/Rooms/Lv1Rooms/crepa.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
 func _on_crepa_body_entered(body):
-	if not body.can_fly:
-		$CollisionPolygon2D.disabled = false
-	else:
-		$CollisionPolygon2D.disabled = true
+	if body.is_in_group("Character"):
+		if not body.can_fly:
+			$CollisionPolygon2D.disabled = false
+		else:
+			$CollisionPolygon2D.disabled = true
