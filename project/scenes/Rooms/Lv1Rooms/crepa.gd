5b4d4a043942e8c840e24713a86e5212c1e43867
Angela Gallo <angelagallo22@gmail.com>
2022-09-06 21:43:00 +0200

    modificato script di crepa

--- project/scenes/Rooms/Lv1Rooms/crepa.gd
+++ project/scenes/Rooms/Lv1Rooms/crepa.gd
@@ -1,9 +1,7 @@
 extends Area2D
 
-
-
 func _on_crepa_body_entered(body):
-	if body.can_fly:
-		$CollisionPolygon2D.disabled = true
-	else:
+	if not body.can_fly:
 		$CollisionPolygon2D.disabled = false
+	else:
+		$CollisionPolygon2D.disabled = true
