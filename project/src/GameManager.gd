a70985f502226cd7a6e3ac023a45f01efb744db9
Massimo Di Gennaro <maxilmigliore2001@gmail.com>
2022-09-07 20:29:22 +0200

    Risolto errore per il fs durante inserimento nome

--- project/src/GameManager.gd
+++ project/src/GameManager.gd
@@ -256,6 +256,7 @@ var stacked_menu
 func ask_name():
 	var stacked_menu = load("res://scenes/menu/NamePopUp.tscn").instance()
 	$HUD.add_child(stacked_menu)
+	get_node("/root/DefaultLoad").is_blocked = true
 
 func save_name(name: String):
 	saved_state.name = name
