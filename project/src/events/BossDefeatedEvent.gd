b814bd374f156844b1ce8043a32f5db40efbe95c
Carlo Corvaglia <corvagliacarlo99@gmail.com>
2022-09-10 18:20:08 +0200

    Eventi rimanenti

--- project/src/events/BossDefeatedEvent.gd
+++ project/src/events/BossDefeatedEvent.gd
@@ -1,14 +1,20 @@
 extends Event
 
-onready var file = preload("res://dialogues/TutorialDialogues.tres")
+export var file_path: String
+var file
 
-export var boss_name: String
-onready var boss: Node = get_parent().get_parent().get_node("Objects/"+boss_name)
+export(Array, String) var boss_names
+var boss: Array
 onready var portal: Node = get_parent().get_parent().get_node("Objects/GotoNextLvl")
 
 export var hp_up: int
 export var mp_up: int
 export var slot_up: bool
+
+func _ready():
+	file = load(file_path)
+	for name in boss_names:
+		 boss.append(get_parent().get_parent().get_node("Objects/" + name))
 
 func start():
 	if not activated:
@@ -28,14 +34,13 @@ func activate():
 	assistant.set_max_mana(assistant.max_mana + mp_up)
 	assistant.add_slot()
 	
-	
-	
 	DialogueManager.game_states = [portal]
 	DialogueManager.show_example_dialogue_balloon("Boss", file)
 
 func load_event(status: bool):
 	.load_event(status)
 	if is_ever_used:
-		if is_instance_valid(boss):
-			boss.queue_free()
+		for b in boss:
+			if is_instance_valid(b):
+				b.queue_free()
 		portal.activate_portal()
